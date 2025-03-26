32459ef082adec57d618e77b1a0319143e7ba76e
unknown <ebubeokonkwo690@gmail.com>
2025-03-26 15:48:51 +0100

    home text edit

--- src/app/(root)/page.js
+++ src/app/(root)/page.js
@@ -3,12 +3,12 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="">
-       <section className=" bg-primary">
-
+       <section className="pink_container bg-orange-600">
+          <h1 className="heading">
+          Pitch Your Startup<br/> Connect With Entrepreneurs
+          </h1>
        </section> 
-       <h1 className="heading">
-         Home
-        </h1>
+       
     </div>
   );
 }
