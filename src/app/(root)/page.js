f0b91ffe4776bf568e608524e98e09787c0022b5
unknown <ebubeokonkwo690@gmail.com>
2025-03-26 15:26:10 +0100

    Initial commit

---
+++ src/app/(root)/page.js
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="">
+       <section className=" bg-primary">
+
+       </section> 
+       <h1 className="heading">
+         Home
+        </h1>
+    </div>
+  );
+}
